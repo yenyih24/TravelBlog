3e151eb1aec3b938273886e8656e5403703c33b5
Yen-Yi Hsu <hsu00026@algonquinlive.com>
2024-11-20 23:08:49 -0500

    update the format for edit.php

--- pages/edit.php
+++ pages/edit.php
@@ -1,3 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name=" description " content=" Assignment2 " />
+    <!-- Add authorship metadata and link CSS and JS files -->
+
+    <title>Group Project - Travel blog- Edit page</title>
+    <!-- <script src="script.js" defer></script> -->
+    <link rel="stylesheet" type="text/css" href="../css/form.css">
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+
+</head>
+
 <!-- single page form so we get the id and if we hit post the we update so we will process the update mysqli_query
 and redirect to show page otherwise just display the record. -->
 <?php
@@ -38,7 +53,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   $result = mysqli_query($db, $sql);
 
   // 重新導向至顯示頁面
-  header("Location: blog.php?post_id=$post_id");
+  header("Location: index.php?post_id=$id");
+  exit;
 } else {
   // 顯示目前的 post 資訊
   $sql = "SELECT * FROM post WHERE post_id='$id'";
@@ -47,29 +63,32 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 }
 ?>
 
+<div class="form-container">
 
-<div id="content">
-
-  <div class="page edit">
     <h1>Edit Post</h1>
 
     <form action="<?php echo 'edit.php?id=' . $result['post_id']; ?>" method="post" >
-    <dl>
-        <dt>Title</dt>
-        <dd><input type="text" name="title" value="<?php echo $result['title']; ?>" /></dd>
-      </dl>
-      <dl>
-        <dt>State</dt>
-        <dd><input type="text" name="state" value="<?php echo $result['state']; ?>" /></dd>
-      </dl>
-      <dl>
-        <dt>Country</dt>
-        <dd><input type="text" name="country" value="<?php echo $result['country']; ?>" /></dd>
-      </dl>
-      <dl>
-        <dt>Content</dt>
-        <dd><textarea name="post_content"><?php echo $result['content']; ?></textarea></dd>
-      </dl>
+
+    <div class="textfield">
+          <label for="title">Title : </label>
+          <input type="text" name="title" value="<?php echo $result['title']; ?>" />
+    </div>
+
+    <div class="textfield">
+          <label for="title">State : </label>
+          <input type="text" name="state" value="<?php echo $result['state']; ?>" />
+    </div>
+
+    <div class="textfield">
+          <label for="title">Country : </label>
+          <input type="text" name="country" value="<?php echo $result['country']; ?>" />
+    </div>
+
+    <div class="content">
+          <label for="title">Content : </label>
+          <textarea name="post_content"><?php echo $result['content']; ?></textarea>
+    </div>
+  
       <dl>
         <dt>Image</dt>
         <dd><input type="file" name="image" /></dd>
@@ -80,6 +99,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
       </div>
     </form>
   </div>
-</div>
+
 
 <?php include 'footerTB.php'; ?>
+
+</body>
+
+</html>
